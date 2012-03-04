086ad04698d337ba3b4505b320e97eb1c4360e84
Zed A. Shaw <zedshaw@zedshaw.com>
2012-03-04 17:13:46 -0600

    Clean up suffix array tests.

--- tests/sarray_tests.c
+++ tests/sarray_tests.c
@@ -1,20 +1,32 @@
 #include "minunit.h"
 #include <lcthw/sarray.h>
 
-char *test_create_destroy()
+static SuffixArray *sarry = NULL;
+static char test[] = "abracadabra";
+static char test2[] = "acadabra";
+
+char *test_create()
 {
-    char test[] = "abracadabra";
-    char test2[] = "acadabra";
-    SuffixArray *sarry = SuffixArray_create(test, sizeof(test));
+    sarry = SuffixArray_create(test, sizeof(test));
     mu_assert(sarry, "Failed to create.");
 
+    return NULL;
+}
+
+char *test_destroy()
+{
+    SuffixArray_destroy(sarry);
+
+    return NULL;
+}
+
+char *test_find_suffix()
+{
     int at = SuffixArray_find_suffix(sarry, test2, sizeof(test2));
     mu_assert(at != -1, "Failed to find the suffix.");
 
     at = SuffixArray_find_suffix(sarry, "yo", sizeof("yo"));
     mu_assert(at == -1, "Should fail to find yo.");
-
-    SuffixArray_destroy(sarry);
 
     return NULL;
 }
@@ -24,7 +36,9 @@ char *test_create_destroy()
 char *all_tests() {
     mu_suite_start();
 
-    mu_run_test(test_create_destroy);
+    mu_run_test(test_create);
+    mu_run_test(test_find_suffix);
+    mu_run_test(test_destroy);
 
     return NULL;
 }

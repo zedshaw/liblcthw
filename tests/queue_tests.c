6b86f272599b419ba64968abb2056882dbb978b7
Zed A. Shaw <zedshaw@zedshaw.com>
2012-03-04 17:05:39 -0600

    Clean up queue tests.

--- tests/queue_tests.c
+++ tests/queue_tests.c
@@ -2,54 +2,47 @@
 #include <lcthw/queue.h>
 #include <assert.h>
 
-char *test_create_destroy()
+static Queue *queue = NULL;
+char *tests[] = {"test1 data", "test2 data", "test3 data"};
+#define NUM_TESTS 3
+
+
+char *test_create()
 {
-    Queue *queue = Queue_create();
+    queue = Queue_create();
     mu_assert(queue != NULL, "Failed to create queue.");
 
-    Queue_clear(queue); // should do nothing
-    Queue_destroy(queue);
+    return NULL;
+}
 
-    queue = Queue_create();
+char *test_destroy()
+{
     mu_assert(queue != NULL, "Failed to make queue #2");
-    Queue_clear_destroy(queue);
+    Queue_destroy(queue);
 
     return NULL;
 }
 
 char *test_send_recv()
 {
-    Queue *queue = Queue_create();
-    mu_assert(queue != NULL, "Failed to create test queue.");
-    char *test1 = "test1 data";
-    char *test2 = "test2 data";
-    char *test3 = "test3 data";
+    int i = 0;
+    for(i = 0; i < NUM_TESTS; i++) {
+        Queue_send(queue, tests[i]);
+        mu_assert(Queue_peek(queue) == tests[0], "Wrong next value.");
+    }
 
-    Queue_send(queue, test1);
-    mu_assert(Queue_peek(queue) == test1, "Wrong last value.");
-
-    Queue_send(queue, test2);
-    mu_assert(Queue_peek(queue) == test1, "Wrong last value");
-
-    Queue_send(queue, test3);
-    mu_assert(Queue_peek(queue) == test1, "Wrong last value.");
-    mu_assert(Queue_count(queue) == 3, "Wrong count on push.");
+    mu_assert(Queue_count(queue) == NUM_TESTS, "Wrong count on send.");
 
     QUEUE_FOREACH(queue, cur) {
         debug("VAL: %s", (char *)cur->value);
     }
 
-    char *val = Queue_recv(queue);
-    mu_assert(val == test1, "Wrong value on pop.");
+    for(i = 0; i < NUM_TESTS; i++) {
+        char *val = Queue_recv(queue);
+        mu_assert(val == tests[i], "Wrong value on recv.");
+    }
 
-    val = Queue_recv(queue);
-    mu_assert(val == test2, "Wrong value on pop.");
-
-    val = Queue_recv(queue);
-    mu_assert(val == test3, "Wrong value on pop.");
-    mu_assert(Queue_count(queue) == 0, "Wrong count after pop.");
-
-    Queue_destroy(queue);
+    mu_assert(Queue_count(queue) == 0, "Wrong count after recv.");
 
     return NULL;
 }
@@ -57,8 +50,9 @@ char *test_send_recv()
 char *all_tests() {
     mu_suite_start();
 
-    mu_run_test(test_create_destroy);
+    mu_run_test(test_create);
     mu_run_test(test_send_recv);
+    mu_run_test(test_destroy);
 
     return NULL;
 }

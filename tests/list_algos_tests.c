fc41e575f2fbff848900feca3b3a6967f64bb66a
Zed A. Shaw <zedshaw@zedshaw.com>
2012-04-09 17:25:13 -0700

    A little more testing of the merge sort.

--- tests/list_algos_tests.c
+++ tests/list_algos_tests.c
@@ -65,7 +65,11 @@ char *test_merge_sort()
     List *res = List_merge_sort(words, (List_compare)strcmp);
     mu_assert(is_sorted(res), "Words are not sorted after merge sort.");
 
-    List_destroy(res); 
+    List *res2 = List_merge_sort(res, (List_compare)strcmp);
+    mu_assert(is_sorted(res), "Should still be sorted after merge sort.");
+    List_destroy(res2);
+    List_destroy(res);
+
     List_destroy(words);
     return NULL;
 }

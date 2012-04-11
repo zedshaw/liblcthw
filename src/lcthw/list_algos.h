7e56885dbd46d1eb53d2082e906a6e2e2d7972af
Zed A. Shaw <zedshaw@zedshaw.com>
2012-04-11 16:42:33 -0700

    Make the compare const void.

--- src/lcthw/list_algos.h
+++ src/lcthw/list_algos.h
@@ -3,7 +3,7 @@
 
 #include <lcthw/list.h>
 
-typedef int (*List_compare)(void *a, void *b);
+typedef int (*List_compare)(const void *a, const void *b);
 
 int List_bubble_sort(List *list, List_compare cmp);
 

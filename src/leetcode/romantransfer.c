1dcd771b88ca43b9fe6043352aaf34b48c25612f
root <root@localhost.localdomain>
2022-04-23 14:21:23 +0800

    modify comment

--- src/leetcode/romantransfer.c
+++ src/leetcode/romantransfer.c
@@ -7,77 +7,61 @@ typedef struct
 } roman_map;
 
 roman_map roman_mapping[] = {
-    {'I', 1},
-    {'V', 5},
-    {'X', 10},
-    {'L', 50},
-    {'C', 100},
-    {'D', 500},
-    {'M', 1000},
+    { 'I', 1 },
+    { 'V', 5 },
+    { 'X', 10 },
+    { 'L', 50 },
+    { 'C', 100 },
+    { 'D', 500 },
+    { 'M', 1000 },
 };
 
 /*
+  https://leetcode-cn.com/problems/roman-to-integer/
   输入: s = "MCMXCIV"
   输出: 1994
   解释: M = 1000, CM = 900, XC = 90, IV = 4.
 */
 
-int romanToInt(char *s)
+int romanToInt(char* s)
 {
-    char *p;
+    char* p;
     int result = 0;
     int type_num = sizeof(roman_mapping) / sizeof(roman_map);
-    for (p = s; *p != '\0'; p++)
-    {
-        if (*p == 'I' && *(p + 1) != '\0')
-        {
-            if (*(p + 1) == 'V')
-            {
+    for (p = s; *p != '\0'; p++) {
+        if (*p == 'I' && *(p + 1) != '\0') {
+            if (*(p + 1) == 'V') {
                 result += 4;
                 p++;
                 continue;
-            }
-            else if (*(p + 1) == 'X')
-            {
+            } else if (*(p + 1) == 'X') {
                 result += 9;
                 p++;
                 continue;
             }
-        }
-        else if (*p == 'X' && *(p + 1) != '\0')
-        {
-            if (*(p + 1) == 'L')
-            {
+        } else if (*p == 'X' && *(p + 1) != '\0') {
+            if (*(p + 1) == 'L') {
                 result += 40;
                 p++;
                 continue;
-            }
-            else if (*(p + 1) == 'C')
-            {
+            } else if (*(p + 1) == 'C') {
                 result += 90;
                 p++;
                 continue;
             }
-        }
-        else if (*p == 'C' && *(p + 1) != '\0')
-        {
-            if (*(p + 1) == 'D')
-            {
+        } else if (*p == 'C' && *(p + 1) != '\0') {
+            if (*(p + 1) == 'D') {
                 result += 400;
                 p++;
                 continue;
-            }
-            else if (*(p + 1) == 'M')
-            {
+            } else if (*(p + 1) == 'M') {
                 result += 900;
                 p++;
                 continue;
             }
         }
-        for (int i = 0; i < type_num; i++)
-        {
-            if (*p == roman_mapping[i].ch)
-            {
+        for (int i = 0; i < type_num; i++) {
+            if (*p == roman_mapping[i].ch) {
                 result += roman_mapping[i].num;
             }
         }

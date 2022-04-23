0fa780dad683921b31b58dd8f190d24e518c7568
hamwastp <hanghua@foxmail.com>
2022-04-24 01:32:17 +0800

    Update zigzagconversion.c
    refer

--- src/leetcode/zigzagconversion.c
+++ src/leetcode/zigzagconversion.c
@@ -43,28 +43,6 @@ s 由英文字母（小写和大写）、',' 和 '.' 组成
 #include <stdlib.h>
 #include <string.h>
 
-// https://blog.csdn.net/qq_38537503/article/details/95031638
-// vector<string> temp(numRows);
-// string res;
-// if (s.empty() || numRows < 1)
-//     return res;
-// if (numRows == 1)
-//     return s;
-// for (int i = 0; i < s.size(); i++) {
-//     int ans = i / (numRows - 1);
-//     int remain = i % (numRows - 1);
-//     if (ans % 2 == 0) { //结果为偶数或0
-//         temp[remain].push_back(s[i]); //按余数正序保存
-//     }
-//     if (ans % 2 != 0) { //结果为奇数
-//         temp[numRows - remain - 1].push_back(s[i]); //按余数倒序保存
-//     }
-// }
-// for (int i = 0; i < temp.size(); i++) {
-//     res += temp[i];
-// }
-// return res;
-
 char* convert(char* s, int numRows)
 {
     DArray* temp;
@@ -146,3 +124,26 @@ char* convert(char* s, int numRows)
 
     return res;
 }
+
+// refer: 
+// https://blog.csdn.net/qq_38537503/article/details/95031638
+// vector<string> temp(numRows);
+// string res;
+// if (s.empty() || numRows < 1)
+//     return res;
+// if (numRows == 1)
+//     return s;
+// for (int i = 0; i < s.size(); i++) {
+//     int ans = i / (numRows - 1);
+//     int remain = i % (numRows - 1);
+//     if (ans % 2 == 0) { //结果为偶数或0
+//         temp[remain].push_back(s[i]); //按余数正序保存
+//     }
+//     if (ans % 2 != 0) { //结果为奇数
+//         temp[numRows - remain - 1].push_back(s[i]); //按余数倒序保存
+//     }
+// }
+// for (int i = 0; i < temp.size(); i++) {
+//     res += temp[i];
+// }
+// return res;
